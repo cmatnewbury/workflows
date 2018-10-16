569c52da721b3f9a284898e133e3d48dcd7fb921
Tina N <usa.tnewbury@cma-cgm.com>
2018-10-16 09:56:26 -0400

    concatenated javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,27 @@
 var gulp = require('gulp'), 
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
-gulp.task('log', function(){
-    gutil.log('Workflows are awesome');
+var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
+];
+
+gulp.task('coffee', function(done){
+    gulp.src(coffeeSources)
+        .pipe(coffee({bare: true})
+            .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'))
+        done();
+});
+
+gulp.task('js', function(done){
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
+        done();
 });
