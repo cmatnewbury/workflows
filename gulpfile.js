1163be4bc3d4913259efcd7b75f6b83acaa80772
Tina N <usa.tnewbury@cma-cgm.com>
2018-10-18 10:31:18 -0400

    added compasss processs

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
+    //rubsass = require('gulp-ruby-sass'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -11,6 +13,7 @@ var jsSources = [
     'components/scripts/tagline.js',
     'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.scss']
 
 gulp.task('coffee', function(done){
     gulp.src(coffeeSources)
@@ -25,5 +28,23 @@ gulp.task('js', function(done){
         .pipe(concat('script.js'))
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
+        done();
+});
+/*gulp.task('compass', function(done){
+    gulp.src(sassSources)
+        .pipe(rubsass())
+        .on('error', gutil.log)
+        .pipe(gulp.dest('builds/development/css'))
+        done();
+});*/
+gulp.task('compass', function(done){
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            image: 'builds/development/images',
+            style: 'expanded'
+        }))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('builds/development/css'))
         done();
 });
